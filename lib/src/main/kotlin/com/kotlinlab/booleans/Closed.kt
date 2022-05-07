a0f275a684f8db2ef16670a8af7a29d814a73c58
alex <alex.pajaron@vier.ai>
2022-05-07 07:41:51 +0200

    add some more examples

---
+++ lib/src/main/kotlin/com/kotlinlab/booleans/Closed.kt
@@ -0,0 +1,10 @@
+package com.kotlinlab.booleans
+
+fun isClosed(hour: Int) {
+    val open = 9
+    val closed = 20
+    println("Operating hours: $open - $closed")
+    val status = hour < open || hour > closed
+    println("Closed: $status")
+}
+fun main() = isClosed(6)
---
+++ lib/src/main/kotlin/com/kotlinlab/booleans/EvaluationOrder.kt
@@ -0,0 +1,21 @@
+package com.kotlinlab.booleans
+
+fun main() {
+    val sunny = true
+    val hoursSleep = 6
+    val exercise = false
+    val temp = 55
+// [1]:
+    val happy1 = sunny && temp > 50 ||
+            exercise && hoursSleep > 7
+    println(happy1)
+// [2]:
+    val sameHappy1 = (sunny && temp > 50) ||
+            (exercise && hoursSleep > 7)
+    println(sameHappy1)
+// [3]:
+    val notSame =
+        (sunny && temp > 50 || exercise) &&
+                hoursSleep > 7
+    println(notSame)
+}
