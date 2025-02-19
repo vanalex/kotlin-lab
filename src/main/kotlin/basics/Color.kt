b091ddf0068acbd66482bdb7f766d04fda9c15cc
vanalex <apjorquera@gmail.com>
2025-02-19 13:44:05 +0100

    add more enum examples

--- src/main/kotlin/basics/Color.kt
+++ src/main/kotlin/basics/Color.kt
@@ -10,6 +10,17 @@ enum class Color(val r: Int, val g: Int, val b: Int) {
     VIOLET(238, 130, 238);
 
     fun rgb() = (r * 256 + g) * 256 + b
+
+    fun measureColor() = ORANGE
+
+    fun getWarmthFromSensor(): String {
+        val color = measureColor()
+        return when(color) {
+            RED, ORANGE, YELLOW -> "warm (red = ${color.r})"
+            GREEN -> "neutral (green = ${color.g})"
+            BLUE, INDIGO, VIOLET -> "cold (blue = ${color.b})"
+        }
+    }
 }
 
 fun getMnemonic(color: Color) =
@@ -21,5 +32,30 @@ fun getMnemonic(color: Color) =
         Color.BLUE -> "Battle"
         Color.INDIGO -> "In"
         Color.VIOLET -> "Vain"
+    }
+
+fun mix (color1: Color, color2: Color) =
+    when (setOf(color1, color2)) {
+        setOf(Color.RED, Color.YELLOW) -> Color.ORANGE
+        setOf(Color.YELLOW, Color.BLUE) -> Color.GREEN
+        setOf(Color.BLUE, Color.VIOLET) -> Color.INDIGO
+        else -> throw Exception("Dirty color")
+    }
+
+fun mixOptimized(c1: Color, c2: Color) =
+    when {
+        (c1 == Color.RED && c2 == Color.YELLOW) ||
+                (c1 == Color.YELLOW && c2 == Color.RED) ->
+            Color.ORANGE
+
+        (c1 == Color.YELLOW && c2 == Color.BLUE) ||
+                (c1 == Color.BLUE && c2 == Color.YELLOW) ->
+            Color.GREEN
+
+        (c1 == Color.BLUE && c2 == Color.VIOLET) ||
+                (c1 == Color.VIOLET && c2 == Color.BLUE) ->
+            Color.INDIGO
+
+        else -> throw Exception("Dirty color")
     }
 
--- src/test/kotlin/basics/ColorTest.kt
+++ src/test/kotlin/basics/ColorTest.kt
@@ -2,7 +2,10 @@ package basics
 
 import org.example.basics.Color
 import org.example.basics.getMnemonic
+import org.example.basics.mix
+import org.example.basics.mixOptimized
 import kotlin.test.Test
+import kotlin.test.assertFailsWith
 
 
 class ColorTest {
@@ -17,4 +20,43 @@ class ColorTest {
         val red = Color.RED
         assert(getMnemonic(red) == "Richard")
     }
+
+    @Test
+    fun `test warmth from sensor`(){
+        val warmth = Color.ORANGE.getWarmthFromSensor()
+        assert(warmth == "warm (red = ${Color.ORANGE.r})")
+    }
+
+    @Test
+    fun `test mix`(){
+        val mix = mix(Color.YELLOW, Color.BLUE)
+        assert(mix == Color.GREEN)
+    }
+
+    @Test
+    fun `test mix throws exception`(){
+        assertFailsWith<Exception>(
+            message = "Dirty Color",
+            block = {
+                mix(Color.YELLOW, Color.VIOLET)
+            }
+        )
+    }
+
+    @Test
+    fun `test mix optmizied throws exception`(){
+        val mix = mix(Color.YELLOW, Color.RED)
+        assert(mix == Color.ORANGE)
+    }
+
+    @Test
+    fun `test mix optimized throws exception`(){
+        assertFailsWith<Exception>(
+            message = "Dirty Color",
+            block = {
+                mixOptimized(Color.ORANGE, Color.VIOLET)
+            }
+        )
+    }
+
 }
